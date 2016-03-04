3c911a603d06e729f6f65c12aa8c3503d745cd73
Kalimaha <guido.barbaglia@gmail.com>
2016-03-04 17:03:16 +1100

    Test initialization.

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -1,3 +1,4 @@
+/*global $*/
 var SIMULATOR = (function () {
 
     'use strict';
@@ -5,10 +6,42 @@ var SIMULATOR = (function () {
     function SIM() {
 
         this.CONFIG = {
-            version: 0.1
+            version: 0.1,
+            table: {
+                length: 5,
+                height: 5
+            },
+            commands: ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT'],
+            position: {
+                x: 0,
+                y: 0,
+                facing: null
+            },
+            command: null
         };
 
     }
+
+    SIM.prototype.init = function (config) {
+
+        /* Extend default configuration. */
+        this.CONFIG = $.extend(true, {}, this.CONFIG, config);
+
+    };
+
+    SIM.prototype.execute = function (command) {
+        if (command === undefined || command === null) {
+            throw new Error('Please provide a valid command.');
+        }
+        command = command.toUpperCase();
+        if (this.CONFIG.command === null && command !== 'PLACE') {
+            throw new Error('The first valid command to the robot must be the PLACE command.');
+        }
+    };
+
+    SIM.prototype.place = function (x, y, facing) {
+
+    };
 
     return SIM;
 
--- test/spec/SimulatorSpec.js
+++ test/spec/SimulatorSpec.js
@@ -9,12 +9,75 @@ describe('Toy Robot Simulator', function () {
         s = new SIMULATOR();
     });
 
-    it('should have a version number', function () {
-        expect(s.CONFIG.version).not.toBeUndefined();
+    describe('Configurability', function () {
+
+        it('The simulator should have a version number', function () {
+            expect(s.CONFIG.version).not.toBeUndefined();
+        });
+
+        it('with current version equal to 0.1', function () {
+            expect(s.CONFIG.version).toEqual(0.1);
+        });
+
+        it('that can be overridden to 0.2', function () {
+            s.init({
+                version: 0.2
+            });
+            expect(s.CONFIG.version).toEqual(0.2);
+        });
+
+        it('The simulator accepts 5 different commands', function () {
+            expect(s.CONFIG.commands.length).toEqual(5);
+        });
+
+        it('The simulator has a table that is 5 units wide', function () {
+            expect(s.CONFIG.table.length).toEqual(5);
+        });
+
+        it('and 5 units long', function () {
+            expect(s.CONFIG.table.height).toEqual(5);
+        });
+
     });
 
-    it('with current version equal to 0.1', function () {
-        expect(s.CONFIG.version).toEqual(0.1);
+    describe('Usage', function () {
+
+        it('The simulation starts with the PLACE command. NULL throws an error.', function () {
+            expect(function () {
+                s.execute(null);
+            }).toThrowError('Please provide a valid command.');
+        });
+
+        it('The simulation starts with the PLACE command. UNDEFINED throws an error.', function () {
+            expect(function () {
+                s.execute();
+            }).toThrowError('Please provide a valid command.');
+        });
+
+        it('The simulation starts with the PLACE command. MOVE throws an error.', function () {
+            expect(function () {
+                s.execute('MOVE');
+            }).toThrowError('The first valid command to the robot must be the PLACE command.');
+        });
+
+        it('The simulation starts with the PLACE command. LEFT throws an error.', function () {
+            expect(function () {
+                s.execute('LEFT');
+            }).toThrowError('The first valid command to the robot must be the PLACE command.');
+        });
+
+        it('The simulation starts with the PLACE command. RIGHT throws an error.', function () {
+            expect(function () {
+                s.execute('RIGHT');
+            }).toThrowError('The first valid command to the robot must be the PLACE command.');
+        });
+
+        it('The simulation starts with the PLACE command. REPORT throws an error.', function () {
+            expect(function () {
+                s.execute('REPORT');
+            }).toThrowError('The first valid command to the robot must be the PLACE command.');
+        });
+
     });
 
 });
